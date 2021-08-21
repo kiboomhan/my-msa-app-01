1fac2e124e4eb61a82d7fc5af7899ad1450eaf58
kiboomhan <kiboomhan@gmail.com>
2021-08-21 22:32:55 +0900

    [#1] 프로젝트 생성 - Spring Boot DevTools 의존성 추가 - Lombok 의존성 추가 - Spring Web 의존성 추가 - Eureka Discovery Client 의존성 추가

---
+++ src/main/java/com/example/msaapp01/MsaApp01Application.java
@@ -0,0 +1,13 @@
+package com.example.msaapp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaApp01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaApp01Application.class, args);
+    }
+
+}
